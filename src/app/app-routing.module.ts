7b44b6bed1a672e34658ae1fb3782dc484703f2e
Valentin <koffix@tutanota.de>
2017-09-25 13:09:54 +0300

    clean

--- src/app/app-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { StoremeComponent } from './storeme/storeme.component';
-
-const router: Routes = [
-  { path: '', redirectTo: 'shops', pathMatch: 'full' },
-  { path: 'shops', component: StoremeComponent }
-];
-export const routes: ModuleWithProviders = RouterModule.forRoot(router);
